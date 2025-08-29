18b911e6207808ebbb7d2a80c0eb979272513b28
saadsafda <muhammadsaadsafdar2005@gmail.com>
2025-08-29 09:38:20 -0700

    add homepage api

--- cotton_valley/api/homepage.py
+++ cotton_valley/api/homepage.py
@@ -8,25 +8,25 @@ def get_homepage_slides():
     result = {
         "content": {
             "slider_products": {
-                "status": true,
+                "status": True,
                 "product_slider_1": {
                     "title": doc.first_title,
-                    "status": true,
+                    "status": True,
                     "product_ids": [row.product for row in doc.slide_1_ids]
                 },
                 "product_slider_2": {
                     "title": doc.sec_title,
-                    "status": true,
+                    "status": True,
                     "product_ids": [row.product for row in doc.slide_2_ids]
                 },
                 "product_slider_3": {
                     "title": doc.third_title,
-                    "status": true,
+                    "status": True,
                     "product_ids": [row.product for row in doc.slide_3_ids]
                 },
                 "product_slider_4": {
                     "title": doc.forth_title,
-                    "status": true,
+                    "status": True,
                     "product_ids": [row.product for row in doc.slide_4_ids]
                 },
             }
