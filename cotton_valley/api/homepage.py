4a215a1e33128053b98717c957ef4a6fffa7d5e5
saadsafda <muhammadsaadsafdar2005@gmail.com>
2025-09-01 06:31:21 -0700

    category api

--- cotton_valley/api/homepage.py
+++ cotton_valley/api/homepage.py
@@ -9,7 +9,7 @@ def get_homepage_slides():
         "content": {
             "categories_image_list": {
                 "title": doc.category_title,
-                "category_ids": [d["product_category"] for d in doc.homepage_categories],
+                "category_ids": [d.product_category for d in doc.homepage_categories],
                 "status": doc.show_categories
             },
             "slider_products": {
