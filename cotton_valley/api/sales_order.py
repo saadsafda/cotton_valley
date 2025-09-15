ac4893e10943a5ced7f9eef9b98e7a0ca509a496
saadsafda <muhammadsaadsafdar2005@gmail.com>
2025-09-15 17:01:17 +0500

    update sales order api

--- cotton_valley/api/sales_order.py
+++ cotton_valley/api/sales_order.py
@@ -22,6 +22,7 @@ def create_or_update_sales_order(items):
 
     if not customer:
         return "Customer not found"
+
     customer_id = customer["id"]
     so = frappe.get_all(
         "Sales Order",
@@ -29,6 +30,7 @@ def create_or_update_sales_order(items):
         fields=["name"],
         limit=1,
     )
+
 
     if so:
         so_doc = frappe.get_doc("Sales Order", so[0].name)
@@ -46,8 +48,8 @@ def create_or_update_sales_order(items):
             "qty": row["qty"],
             "rate": row["rate"],
         })
-
     so_doc.save(ignore_permissions=True)
+    frappe.db.commit()
     return so_doc.name
 
     
