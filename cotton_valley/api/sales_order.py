214c12124dab232609df284afab260ab417f2ea7
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-10-03 19:15:57 +0500

    update

--- cotton_valley/api/sales_order.py
+++ cotton_valley/api/sales_order.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import frappe # type: ignore
 from frappe.utils import nowdate # type: ignore
 from cotton_valley.api.customer import get_current_customer
