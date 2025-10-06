3ab28c26e78b93aab79825b19b54bb53e433f6cb
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-10-06 13:54:48 +0500

    updates

--- cotton_valley/cotton_valley/doctype/customer_token/customer_token.py
+++ cotton_valley/cotton_valley/doctype/customer_token/customer_token.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2025, Saad and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class CustomerToken(Document):
-	pass
+	def validate(self):
+		if not self.customer:
+			frappe.throw("Customer is required")
+
+		# Count other tokens safely
+		token_count = frappe.db.count(
+			"Customer Token", 
+			filters={"customer": self.customer}
+		)
+
+		# Lock customer row to prevent race conditions
+		frappe.db.sql("SELECT name FROM `tabCustomer` WHERE name=%s FOR UPDATE", self.customer)
+
+		# Update the no_of_login field
+		frappe.db.set_value("Customer", self.customer, "no_of_login", token_count)
+
+		
