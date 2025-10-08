e81962af5fec686dee8dc6f9e5f422fdfcf762aa
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-10-08 19:05:20 +0500

    make delivery note with order submit

--- cotton_valley/server_scripts/sales_order.py
+++ cotton_valley/server_scripts/sales_order.py
@@ -25,3 +25,30 @@ def update_customer_order_summary(doc, method):
     })
 
     frappe.db.commit()
+
+    make_delivery_note_on_submit(doc, method)
+
+
+def make_delivery_note_on_submit(doc, method):
+    try:
+        dn = frappe.new_doc("Delivery Note")
+        dn.customer = doc.customer
+        dn.company = doc.company
+        dn.posting_date = frappe.utils.nowdate()
+        dn.set_warehouse = doc.set_warehouse or None
+
+        for item in doc.items:
+            dn.append("items", {
+                "item_code": item.item_code,
+                "qty": item.qty,
+                "rate": item.rate,
+                "against_sales_order": doc.name,
+                "so_detail": item.name,
+                "warehouse": item.warehouse,
+            })
+
+        dn.save(ignore_permissions=True)
+        dn.submit()
+        frappe.db.commit()
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "Auto Delivery Note Creation Failed")
