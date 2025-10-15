b093b3c8784d6d4842897236e52d1996de324b94
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-10-15 14:39:06 +0500

    update

--- cotton_valley/api/products.py
+++ cotton_valley/api/products.py
@@ -225,13 +225,14 @@ def get_all_products(ids=None, category=None, subcategory=None, sortBy=None, sea
             WHERE item_code in %s and price_list = %s
         """, (item_ids, price_list), as_dict=True)
         default_price_data = frappe.db.sql("""
-            SELECT price_list_rate
+            SELECT item_code, price_list_rate
             FROM `tabItem Price`
             WHERE item_code in %s and price_list = %s
             LIMIT 1
         """, (item_ids, "Retail"), as_dict=True)
         if not price_data and default_price_data:
             price_data = default_price_data
+
         price_map = {p["item_code"]: p["price_list_rate"] for p in price_data}
 
     # Stock
