a7e9e52a6987f9ca2a2a0a8975c7f5b40df558ef
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-11-05 21:53:17 +0500

    update api

--- cotton_valley/mobile_api/customer.py
+++ cotton_valley/mobile_api/customer.py
@@ -9,7 +9,7 @@ def get_all_customers():
         customers = frappe.get_list("Customer",
             fields=["name", "customer_name", "custom_email_address", "custom_phone_number", "image", "disabled",
                     "mode_of_payment", "sales_person", "custom_company_name", "creation", "modified",
-                    "customer_primary_address", "customer_billing_address", "price_list_for_cv", "price_list_for_udc",
+                    "customer_primary_address", "account_number", "customer_billing_address", "price_list_for_cv", "price_list_for_udc",
                     "no_of_orders", "orders_amount"]
         )
         
