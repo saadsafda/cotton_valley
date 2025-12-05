e0b7ccaae1009f686a0b8dcda14de475147fe773
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-12-05 20:33:49 +0500

    Add image retrieval for subcategories in get_all_categories

--- cotton_valley/mobile_api/category.py
+++ cotton_valley/mobile_api/category.py
@@ -23,7 +23,7 @@ def get_all_categories():
 
         # Batch query for subcategories
         subcategories_data = frappe.db.sql("""
-            SELECT parent, product_subcategory as id, subcategory_name as name
+            SELECT parent, product_subcategory as id, subcategory_name as name, image
             FROM `tabSubCategories`
             WHERE parent IN %s
         """, (category_ids,), as_dict=True)
@@ -33,7 +33,8 @@ def get_all_categories():
         for subcat in subcategories_data:
             subcategories_map.setdefault(subcat["parent"], []).append({
                 "id": subcat["id"],
-                "name": subcat["name"]
+                "name": subcat["name"],
+                "image": frappe.utils.get_url(subcat["image"]) if subcat.get("image") else None
             })
 
         # Attach subcategories to categories
