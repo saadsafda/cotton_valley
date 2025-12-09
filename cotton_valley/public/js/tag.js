cd2c56e020bd9c13f7e63eb9d08eab34cb305bff
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2025-12-09 16:20:10 +0500

    feat: Enhance Tag doctype with refresh function to set product name query based on company

--- cotton_valley/public/js/tag.js
+++ cotton_valley/public/js/tag.js
@@ -1,4 +1,15 @@
 frappe.ui.form.on('Tag', {
+    refresh: function (frm) {
+        if (frm.doc.company) {
+            frm.set_query("product_name", "products", function(doc, cdt, cdn) {
+                return {
+                    filters: {
+                        company: doc.company
+                    }
+                };
+            });
+        }
+    },
     company: function (frm) {
         if (frm.doc.company) {
             frm.set_query("product_name", "products", function(doc, cdt, cdn) {
