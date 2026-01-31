b29da1852bff50c57c61b38f7be400869a6dc7f0
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2026-01-31 05:01:38 +0500

    fix: Update URLs for reports and authentication to reflect new domain

--- cotton_valley/public/js/customer.js
+++ cotton_valley/public/js/customer.js
@@ -33,10 +33,10 @@ frappe.ui.form.on('Customer', {
 
                                         // Open new window and set cookie
                                         // Open portal in new tab
-                                        window.open(`https://cottonvalleyllc.vercel.app/auth/erplogin?token=${token}`, "_blank");
+                                        window.open(`https://www.cottonvalley.net/auth/erplogin?token=${token}`, "_blank");
 
                                     } else {
-                                        window.open(`https://universaldcllc.vercel.app/auth/erplogin?token=${token}`, "_blank");
+                                        window.open(`https://www.universaldc.com/auth/erplogin?token=${token}`, "_blank");
                                     }
                                 } else {
                                     frappe.msgprint(__('Login failed: ' + (r.message || 'Unknown error')));
--- cotton_valley/public/js/item.js
+++ cotton_valley/public/js/item.js
@@ -39,9 +39,9 @@ frappe.ui.form.on('Item', {
                 let company_name = frm.doc.company;
 
                 if (company_name == "UDC") {
-                    base_url = "https://universal.destrotechnologies.website/product/"; 
+                    base_url = "https://www.universaldc.com/product/"; 
                 } else {
-                    base_url = "https://cottonvalley.destrotechnologies.website/product/";
+                    base_url = "https://www.cottonvalley.net/product/";
                 }
 
                 if (base_url) {
