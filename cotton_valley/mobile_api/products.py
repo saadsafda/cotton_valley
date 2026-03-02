7db6e97808a7f4db0cecbe31aa20bf21af9c2cdc
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2026-03-02 15:08:43 +0500

    Update report JSON fixtures, modify product filtering logic, and enhance tag functionality
    - Updated the "modified" timestamps and JSON structures in report.json for various reports.
    - Changed the filter condition in get_all_products_with_price_levels to use 'disabled_in_app' instead of 'disabled'.
    - Added 'product_type' to the fields retrieved in get_tag_info function.
    - Enhanced the tag.js script to filter products based on both 'company' and 'product_type' in the Tag form.

--- cotton_valley/mobile_api/products.py
+++ cotton_valley/mobile_api/products.py
@@ -11,7 +11,7 @@ def get_all_products_with_price_levels(product_ids=None, company=None, category=
     try:
         product_ids = None if not product_ids or product_ids == "null" else get_categories_from_string(product_ids)
         # Build filter conditions
-        filter_conditions = ["i.disabled = 0"]
+        filter_conditions = ["i.disabled_in_app = 0"]
         filter_values = []
         
         if product_ids:
--- cotton_valley/mobile_api/tag.py
+++ cotton_valley/mobile_api/tag.py
@@ -14,7 +14,7 @@ def get_tag_info():
 
         tag_list = frappe.db.get_all(
             "Tag",
-            fields=["name", "company", "description"],
+            fields=["name", "company", "product_type", "description"],
         )
 
         if not tag_list:
