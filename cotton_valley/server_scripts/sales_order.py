2ef2b9aa98a2dc359d60692915d36b9f28c1add7
Muhammad Saad <72243425+saadsafda@users.noreply.github.com>
2026-06-02 20:24:19 +0500

    update submit datetime

--- cotton_valley/server_scripts/sales_order.py
+++ cotton_valley/server_scripts/sales_order.py
@@ -86,7 +86,7 @@ def update_customer_order_summary(doc, method):
     # doc.total_orders = total_orders
     # doc.total_order_amount = total_amount
     doc.custom_last_order_date = max((so.transaction_date for so in sales_orders), default=None)
-    
+    doc.submit_datetime = frappe.utils.now()
     # Update Customer fields
     frappe.db.set_value("Customer", doc.customer, {
         "no_of_orders": total_orders,
